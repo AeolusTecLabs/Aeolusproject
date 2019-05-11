c6f2a6ed15b8e7f531792fafb891614557983bc4
raghujohn <raghukannan786@gmail.com>
2019-05-11 19:04:19 +0530

    first commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          welcome to homepage..
+          welcome to my homepage...
       </header>
     </div>
   );
