67f4d066d29c0ff95959540685084d8492d54e85
AeolusTechLabs <aeolustechlabs2011@gmail.com>
2019-05-12 19:15:23 +0530

    first commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          welcome to my homepage...
+          welcome to my homepage
       </header>
     </div>
   );
